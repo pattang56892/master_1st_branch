90bd1750d31dfc552a89002b924c8ac8de5380a0
PT_backup_Master <https://github.com/pattang56892/PT_backup_Master.git>
2023-08-22 01:13:30 -0400

    get a square root / new-feature-draft

---
+++ 05_GET_A_Square_Root.py
@@ -0,0 +1,6 @@
+# This is a code for getting a square root
+num = float(input('Please input a number ： '))
+print (num)
+sqrt = num ** 0.5
+print('\n The square root of %0.1f is: %0.4f'%(num
+, sqrt))
