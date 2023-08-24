719f87a9f7f49a4d91fbcdd4e9048ea23bacd5ab
PT_backup_Master <https://github.com/pattang56892/PT_backup_Master.git>
2023-08-24 14:18:33 -0400

    added 06_Calculate_Trangle_area.

---
+++ 06_Cal_Triangle_Area.py
@@ -0,0 +1,27 @@
+# print('Please input three numbers: ')  # three sides of a triangle
+# x = float(input(''))
+# y = float(input(''))
+# z = float(input(''))
+# print('%f ' %x)
+# print('%f ' %y)
+# print('%f ' %z)
+# # calculate the half girth
+# girth = (x + y + z) / 2
+# # calculate the triangle area
+# area = (girth*(girth-x)*(girth-y)*(girth-z)) ** 0.5
+# print('The triangle area is: %0.3f' %area)
+
+
+print('Please input three numbers: ')  # three sides of a triangle
+numbers = input('').split()  # Split the input into a list of strings
+x = float(numbers[0])  # Convert the first string to float
+y = float(numbers[1])  # Convert the second string to float
+z = float(numbers[2])  # Convert the third string to float
+print('%f ' % x)
+print('%f ' % y)
+print('%f ' % z)
+# calculate the half girth
+girth = (x + y + z) / 2
+# calculate the triangle area
+area = (girth * (girth - x) * (girth - y) * (girth - z)) ** 0.5
+print('The triangle area is: %0.3f' % area)
